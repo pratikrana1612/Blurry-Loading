61b54bcba143b2182e4e2b6d08f81751a08e9ee7
Rana Pratik <ranapratik1612@gmail.com>
2022-08-13 21:20:22 +0530

    Blurry Loading

---
+++ app.js
@@ -0,0 +1,26 @@
+const h1 = document.querySelector('.number');
+const per = h1.nextElementSibling;
+const img = document.querySelector('.img');
+
+let count=1;
+let filter=60;
+let opacity=1;
+const counter = () =>
+{
+    if(count>=100)
+    {
+        clearInterval(myInterVal);
+        return;
+    }
+        count++;
+        h1.textContent=count;
+        opacity = opacity - 0.01;
+        h1.style.opacity  = `${opacity}`;
+        per.style.opacity  = `${opacity}`;
+        if(filter>=0)
+        {
+            img.style.filter = `blur(${filter}px)`;
+            filter--;
+        }
+    }
+const myInterVal = setInterval(counter,50);
